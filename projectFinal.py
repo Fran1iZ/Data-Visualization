c6aef01b828767ca11156cb4aa4fdf008ca44225
Fran1iZ <francis.carujo@gmail.com>
2020-01-15 17:04:25 +0000

    HEROKU

--- projectFinal.py
+++ projectFinal.py
@@ -959,5 +959,7 @@ def updateCountryandYear(country,year):
     figBar = go.Figure(data=data, layout=layout)
     return figBar
 
+server = app.server
+
 if __name__=="__main__":
     app.run_server(debug=True)
